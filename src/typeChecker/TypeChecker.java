2951c584c2014699b5973bd3462e729043ba9626
hlokomani-tuks <133780278+hlokomani-tuks@users.noreply.github.com>
2024-10-14 16:55:03 +0200

    Stubbed empty type checker to remove errors

--- src/typeChecker/TypeChecker.java
+++ src/typeChecker/TypeChecker.java
@@ -13,31 +13,37 @@ public class TypeChecker {
 
     public boolean typecheckProg() {
 
+        return false;
     }
 
     public boolean typecheckGLOBVARS() {
         //Case 1: GLOBVARS -> 
 
         //Case 2: GLOBVARS -> VTYP VNAME , GLOBVARS
+        return false;
     }
 
     public boolean typecheckVTYP() {
         //Case 1: VTYP -> num
         //Case 2: VTYP -> text
+        return false;
     }
     
     public String typeOfVNAME() {
 
+        return "";
     }
     
     public boolean typecheckALGO() {
         // ALGO -> begin INSTRUC end
+        return false;
     }
 
     public boolean typecheckINSTRUC() {
         //Case 1: INSTRUC -> 
 
         //Case 2: INSTRUC -> COMMAND ; INSTRUC
+        return false;
     }
 
     public boolean typecheckCOMMAND() {
@@ -54,24 +60,28 @@ public class TypeChecker {
         //Case 6: COMMAND -> CALL
 
         //Case 7: COMMAND -> BRANCH
+        return false;
     }
 
     public boolean typecheckATOMIC() {
         //Case 1: ATOMIC -> VNAME
 
         //Case 2: ATOMIC -> CONST
+        return false;
     }
 
     public boolean typecheckCONST() {
         //Case 1: CONST -> TokenN
 
         //Case 2: CONST -> TokenT
+        return false;
     }
 
     public boolean typecheckASSIGN() {
         //Case 1: ASSIGN -> VNAME < input
         
         //Case 2: ASSIGN -> VNAME := TERM
+        return false;
     }
 
     public boolean typecheckTERM() {
@@ -80,24 +90,29 @@ public class TypeChecker {
         //Case 2: TERM -> CALL
 
         //Case 3: TERM -> OP
+        return false;
     }
 
     public boolean typecheckCALL() {
+        return false;
     }
 
     public boolean typecheckARG() {
         //Case 1: ARG -> ATOMIC
 
         //Case 2: ARG -> OP
+        return false;
     }
 
     public boolean typecheckUNOP() {
         //Case 1: UNOP -> not
 
         //Case 2: UNOP -> sqrt
+        return false;
     }
 
     public boolean typecheckOP() {
+        return false;
     }
 
     public boolean typecheckBINOP() {
@@ -116,60 +131,75 @@ public class TypeChecker {
         //Case 7: BINOP -> mul
 
         //Case 8: BINOP -> div
+        return false;
     }
 
     public boolean typecheckBRANCH() {
+        return false;
     }
 
     public boolean typecheckCOND() {
         //Case 1: COND -> SIMPLE
 
         //Case 2: COND -> COMPOSIT
+        return false;
     }
 
     public boolean typecheckSIMPLE() {
+        return false;
     }
 
     public boolean typecheckCOMPOSIT() {
         //Case 1: COMPOSIT -> BINOP ( SIMPLE , SIMPLE )
 
         //Case 2: COMPOSIT -> UNOP ( SIMPLE )
+        return false;
     }
 
     public String typeOfFNAME() {
+        return "";
     }
 
     public boolean typecheckFUNCTIONS() {
         //Case 1: FUNCTIONS -> 
 
         //Case 2: FUNCTIONS -> DECL FUNCTIONS
+        return false;
     }
 
     public boolean typecheckDECL() {
+        return false;
     }
 
     public boolean typecheckHEADER() {
+        return false;
     }
 
     public boolean typecheckFTYP() {
         //Case 1: FTYP -> num 
 
         //Case 2: FTYP -> void 
+        return false;
     }
 
     public boolean typecheckBODY() {
+        return false;
     }
     
     public boolean typecheckPROLOG() {
+        return false;
     }
 
     public boolean typecheckEPILOG() {
+        return false;
     }
 
     public boolean typecheckLOCVARS() {
+        return false;
     }
 
     public boolean typecheckSUBFUNCS() {
+        return false;
     }
 }
 
