8bb8980d5318839d51e564fdcb1711c59b431ee8
hlokomani-tuks <133780278+hlokomani-tuks@users.noreply.github.com>
2024-10-15 16:37:51 +0200

    Added type information to the symbols

--- src/semanticanalyzer/Symbol.java
+++ src/semanticanalyzer/Symbol.java
@@ -3,16 +3,22 @@ package semanticanalyzer;
 public class Symbol {
     private String name;
     private String kind;
+    private String type;
     private int nodeId;
 
-    public Symbol(String name, String kind, int nodeId) {
+    public Symbol(String name, String kind, String type, int nodeId) {
         this.name = name;
         this.kind = kind;
+        this.type = type;
         this.nodeId = nodeId;
     }
 
     public String getName() {
         return name;
+    }
+
+    public String getType() {
+        return type;
     }
 
     public String getKind() {
